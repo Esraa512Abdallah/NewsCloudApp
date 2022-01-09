b2a1bb41422ee9d751367a147513ccbf5fc0c00f
Esraa512Abdallah <esraaabdallah512@gmail>
2022-01-09 13:36:50 -0800

    first commit

--- lib/helper/constans.dart
+++ lib/helper/constans.dart
@@ -1,29 +1,6 @@
 
 import 'package:flutter/material.dart';
 
-/*final Map<String,String> countrymap = {
-  "Egypt":"eg",
-  "Argentina":"ar",
-  "Austria":"at",
-  "Canada":"ca",
-  "China":"cn",
-};
-   List<String> Countylist =[
-  "Egypt",
-  "Argentina",
-  "Austria",
-  "Canada",
-  "China",
-];
-
- List<String>Countryvalue = [
-   "eg",
-   "ar",
-   "at",
-   "ca",
-  "cn",
-
- ];*/
 
   String country = 'us' ;
   final String ApiKey = 'b870e143651b4baeb3779733311ee712';
